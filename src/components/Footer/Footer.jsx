b28589dd7f8b10ede1cef7272888784204bfe169
amh6281 <amh6281@naver.com>
2022-11-28 16:05:56 +0900

    Footer Design 2022.11.28

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>CATEGORY</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessory</span>
+          <span>Children</span>
+        </div>
+        <div className="item">
+          <h1>COMMUNITY</h1>
+          <span>FAQ</span>
+          <span>Page</span>
+          <span>Store</span>
+        </div>
+        <div className="item">
+          <h1>ABOUT</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            asperiores explicabo perferendis assumenda reprehenderit? Magni
+            temporibus, dolore libero perspiciatis laudantium ipsum, nemo
+            adipisci voluptatum aut tempora, quod eius velit nam.
+          </span>
+        </div>
+        <div className="item">
+          <h1>CONTACT</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+            asperiores explicabo perferendis assumenda reprehenderit? Magni
+            temporibus, dolore libero perspiciatis laudantium ipsum, nemo
+            adipisci voluptatum aut tempora, quod eius velit nam.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">ReactStore</span>
+          <span className="copyright">
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
