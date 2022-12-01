d9c71add3162bad5fb12d1cee7999783af9fcf8c
amh6281 <amh6281@naver.com>
2022-12-01 18:05:51 +0900

    Client strapi connect 2022.12.01
    strapi에 filed와 entry를 추가하고, react에서 연동 후 products의 데이터를 fetch한다.

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,19 +1,39 @@
 import React from "react";
 import "./Card.scss";
 import { Link } from "react-router-dom";
+
 const Card = ({ item }) => {
   return (
     <Link className="link" to={`/product/${item.id}`}>
       <div className="card">
         <div className="image">
-          {item.isNew && <span>신규 상품</span>}
-          <img src={item.img} alt="" className="mainImg" />
-          <img src={item.img2} alt="" className="secondImg" />
+          {item?.attributes.isNew && <span>신규 상품</span>}
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item.attributes?.img?.data?.attributes?.url
+            }
+            alt=""
+            className="mainImg"
+          />
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item.attributes?.img2?.data?.attributes?.url
+            }
+            alt=""
+            className="secondImg"
+          />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item?.attributes.title}</h2>
         <div className="prices">
-          <h3>{new Intl.NumberFormat().format(item.oldPrice)}원</h3>
-          <h3>{new Intl.NumberFormat().format(item.price)}원</h3>
+          <h3>
+            {new Intl.NumberFormat().format(
+              parseInt(item?.attributes.price) + 10000
+            )}
+            원
+          </h3>
+          <h3>{new Intl.NumberFormat().format(item?.attributes.price)}원</h3>
         </div>
       </div>
     </Link>
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,50 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card/Card";
+import axios from "axios";
 import "./FeaturedProducts.scss";
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "롱 슬리브 그래픽 티셔츠",
-      isNew: true,
-      oldPrice: 24900,
-      price: 13900,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "코트",
-      isNew: true,
-      oldPrice: 24900,
-      price: 13900,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "셔츠",
-      isNew: false,
-      oldPrice: 24900,
-      price: 13900,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "모자",
-      isNew: false,
-      oldPrice: 24900,
-      price: 13900,
-    },
-  ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          process.env.REACT_APP_API_URL + "/products?populate=*",
+          {
+            headers: {
+              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+            },
+          }
+        );
+        setProducts(res.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(products);
   return (
     <div className="featuredProducts">
       <div className="top">
         <h1>{type} 상품</h1>
       </div>
       <div className="bottom">
-        {data.map((item) => (
+        {products.map((item) => (
           <Card item={item} key={item.id} />
         ))}
       </div>
