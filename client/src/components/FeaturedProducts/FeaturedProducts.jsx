d3943f9fbe79952fa27f61f6721e06d49ba76dfb
amh6281 <amh6281@naver.com>
2022-12-02 14:19:15 +0900

    custom hook 생성 2022.12.02
    data fetch를 custom hook으로 생성하여 간편화한다.

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,40 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Card from "../Card/Card";
-import axios from "axios";
 import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
-  const [products, setProducts] = useState([]);
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          process.env.REACT_APP_API_URL +
-            `/products?populate=*&[filters][type][$eq]=${type}`,
-          {
-            headers: {
-              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
-            },
-          }
-        );
-        setProducts(res.data.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
-  console.log(products);
   return (
     <div className="featuredProducts">
       <div className="top">
         {type === "recommend" ? <h1>추천 상품</h1> : <h1>트렌드 상품</h1>}
       </div>
       <div className="bottom">
-        {products.map((item) => (
-          <Card item={item} key={item.id} />
-        ))}
+        {error
+          ? "Error!!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
