e95b483e1db8f5a6506a478cfe2e191e8d72c2be
amh6281 <amh6281@naver.com>
2022-12-04 13:47:54 +0900

    Single product data fetch 2022.12.04
    상품의 상세페이지에서 상품의 data를 fetch한다.

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -2,36 +2,53 @@ import React, { useState } from "react";
 import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(1);
 
-  const images = [
-    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrg&w=1600&lazy=load",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrg&w=1600&lazy=load",
-  ];
-
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+  console.log(data);
   return (
     <div className="product">
       <div className="left">
         <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              data?.attributes?.img?.data?.attributes.url
+            }
+            alt=""
+            onClick={(e) => setSelectedImg("img")}
+          />
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              data?.attributes?.img2?.data?.attributes.url
+            }
+            alt=""
+            onClick={(e) => setSelectedImg("img2")}
+          />
         </div>
         <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              data?.attributes[selectedImg]?.data?.attributes?.url
+            }
+            alt=""
+          />
         </div>
       </div>
       <div className="right">
-        <h1>Title</h1>
-        <span className="price">24,900</span>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
-          molestias nam animi necessitatibus ipsa harum vel qui optio officia
-          omnis neque tenetur, commodi impedit dignissimos. Totam at iure
-          consequatur eligendi?
-        </p>
+        <h1>{data?.attributes?.title}</h1>
+        <span className="price">
+          {new Intl.NumberFormat().format(data?.attributes?.price)}
+        </span>
+        <p>{data?.attributes?.desc}</p>
         <div className="quantity">
           <button
             onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
